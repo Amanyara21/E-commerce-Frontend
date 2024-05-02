4411ba22da9b0815d2e25f9bd8cdb03d9b5f7232
Pushkar-Garg <167011407+Pushkar-Garg@users.noreply.github.com>
2024-05-02 21:50:47 +0530

    Update

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -3,12 +3,6 @@ import './NewCollections.css'
 import Item from '../Item/Item'
 
 const NewCollections = () => {
-  const [new_collection,setNew_collection]= useState([])
-  useEffect(()=>{
-    fetch("https://e-commerce-backend-gray.vercel.app/newcollections")
-    .then((response)=> response.json())
-    .then((data)=>setNew_collection(data))
-  },[])
   return (
 
     <div className='new-collections'>
