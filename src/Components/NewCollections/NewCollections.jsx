597c4f4ccab6e498d69ef0164d2b8a3e0be1c472
Pushkar-Garg <167011407+Pushkar-Garg@users.noreply.github.com>
2024-05-02 21:18:36 +0530

    Update

--- src/Components/NewCollections/NewCollections.jsx
+++ src/Components/NewCollections/NewCollections.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './NewCollections.css'
-import new_collection from '../Assets/new_collections'
 import Item from '../Item/Item'
 
 const NewCollections = () => {
   return (
+    const [new_collection,setNew_collection]= useState([])
+  useEffect(()=>{
+    fetch("http://localhost:4000/newcollections")
+    .then((response)=> response.json())
+    .then((data)=>setNew_collection(data))
+  },[])
     <div className='new-collections'>
       <h1>NEW COLLECTIONS</h1>
       <div className="collections">
